f6ad54dd5c09b109dc2273edbdfaa1aee8b4404b
Francis <mfrancis@ohl.com>
2017-07-03 11:50:26 -0500

    Second

---
+++ BotApplication/Dialogs/FlightsDialog.cs
@@ -0,0 +1,18 @@
+﻿
+
+namespace BotApplication.Dialogs
+{
+    using System;
+    using System.Threading.Tasks;
+    using Microsoft.Bot.Builder.Dialogs;
+    using Microsoft.Bot.Connector;
+
+    [Serializable]
+    public class FlightsDialog : IDialog<object>
+    {
+        public async Task StartAsync(IDialogContext context)
+        {
+            context.Fail(new NotImplementedException("Flights Dialog is not implemented and is instead being used to show context.Fail"));
+        }
+    }
+}
